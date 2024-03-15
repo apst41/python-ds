c9fb10b03c01c45a5ede37079e776cf75651c2b1
Ajay Tomar <ajay.pratap@gojek.com>
2024-03-16 00:12:26 +0530

    [Ajay] my first commit

--- array/array_practise.py
+++ array/array_practise.py
@@ -1,35 +1,4 @@
-def threeSum(nums):
-    my_len = len(nums) - 1
-    nums.sort()
-    ans = []
-    
-    for low in range(0, my_len):
-        if low > 0 and nums[low] == nums[low - 1]:
-            continue
-        mid = low + 1
-        high = my_len
-        while mid < high:
-            current = nums[low] + nums[mid] + nums[high]
-            
-            if current == 0:
-                ans.append([nums[low], nums[mid], nums[high]])
-                while mid < high and nums[high] == nums[high - 1]:
-                    high -= 1
-                while mid < high and nums[mid] == nums[mid + 1]:
-                    mid += 1
-                mid += 1
-                high -= 1
-            
-            elif current < 0:
-                mid = mid + 1
-            
-            else:
-                high = high - 1
-    
-    return ans
-
-
 if __name__ == '__main__':
-    my_list = threeSum([-1, 0, 1, 2, -1, -4, -2, -3, 3, 0, 4])
+    my_list = [-1, 0, 1, 2, -1, -4, -2, -3, 3, 0, 4]
     my_list.sort()
     print(my_list)
